f5051aaa15bda83ea119ba5fece759af1fd2154e
ayush20152015 <atyud2015@gmail.com>
2025-06-30 12:19:27 +0530

    updated

---
+++ models/canvasModel.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+
+const canvasSchema=new mongoose.Schema({
+   owner:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true    
+   },
+   shared:[{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+   elements:[{type:mongoose.Schema.Types
+.Mixed}], // no strict data type checking 
+createdAt:{type:Date,default:Date.now}
+
+
+});
+
+module.exports=mongoose.model("Canvas",canvasSchema);
